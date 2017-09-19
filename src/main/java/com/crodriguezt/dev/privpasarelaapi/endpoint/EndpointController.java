2479cb2ca3067d203c47281b8704a7b0479275fd
Cesar Alberto Rodriguez Toledo <cesar.2010.11@gmail.com>
2017-09-19 17:04:24 -0500

    Primer commit

---
+++ src/main/java/com/crodriguezt/dev/privpasarelaapi/endpoint/EndpointController.java
@@ -0,0 +1,31 @@
+package com.crodriguezt.dev.privpasarelaapi.endpoint;
+
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1")
+public class EndpointController {
+	
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@RequestMapping(value = "/test3", method = RequestMethod.GET)
+	  public String endpoint3(@RequestParam Map<String,String> allRequestParams) {
+	    logger.info("\n\n\n\n\n");
+	    logger.info("-> Llego a endpoint2 test2");
+	    logger.info("--> Inicia llamada a UtecResource");
+	    logger.info("---> allRequestParams   " +            allRequestParams);
+	    //UtecResource<String> response = handleSuccessResponse();
+	    //response.setContent("Pago registrado en Netsuite");
+	    String response = "Pago registrado en Netsuite";
+	    return response;
+
+	  }
+
+}
---
+++ src/test/java/com/crodriguezt/dev/privpasarelaapi/PrivPasarelaApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.crodriguezt.dev.privpasarelaapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PrivPasarelaApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
