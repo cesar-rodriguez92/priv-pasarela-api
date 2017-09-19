005cce6ce80fc5a7716ea9b5ebed78bac9e5ba02
Cesar Alberto Rodriguez Toledo <cesar.2010.11@gmail.com>
2017-09-19 17:18:21 -0500

    nuevo endpoint

--- src/main/java/com/crodriguezt/dev/privpasarelaapi/endpoint/EndpointController.java
+++ src/main/java/com/crodriguezt/dev/privpasarelaapi/endpoint/EndpointController.java
@@ -12,20 +12,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/v1")
 public class EndpointController {
-	
-	private final Logger logger = LoggerFactory.getLogger(this.getClass());
-	
-	@RequestMapping(value = "/test3", method = RequestMethod.GET)
-	  public String endpoint3(@RequestParam Map<String,String> allRequestParams) {
-	    logger.info("\n\n\n\n\n");
-	    logger.info("-> Llego a endpoint2 test2");
-	    logger.info("--> Inicia llamada a UtecResource");
-	    logger.info("---> allRequestParams   " +            allRequestParams);
-	    //UtecResource<String> response = handleSuccessResponse();
-	    //response.setContent("Pago registrado en Netsuite");
-	    String response = "Pago registrado en Netsuite";
-	    return response;
 
-	  }
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@RequestMapping(value = "/test3", method = RequestMethod.GET)
+	public String endpoint3(@RequestParam Map<String, String> allRequestParams) {
+		logger.info("\n\n\n\n\n");
+		logger.info("-> Llego a endpoint2 test2");
+		logger.info("--> Inicia llamada a UtecResource");
+		logger.info("---> allRequestParams   " + allRequestParams);
+		// UtecResource<String> response = handleSuccessResponse();
+		// response.setContent("Pago registrado en Netsuite");
+		String response = "Pago registrado en Netsuite";
+		return response;
+
+	}
+
+	@RequestMapping(value = "/test4", method = RequestMethod.GET)
+	public String enpoint4() {
+		System.out.println("SYSOUT: llego a endpoint 4");
+		logger.info("logger: llego a endpoint 4");
+
+		return "Respuesta exitosa";
+	}
 
 }
