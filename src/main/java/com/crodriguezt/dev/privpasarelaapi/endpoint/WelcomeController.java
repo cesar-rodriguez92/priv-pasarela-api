421e11c3f20223944c59447ed6efa7ec6c09340a
Cesar Alberto Rodriguez Toledo <cesar.2010.11@gmail.com>
2017-10-11 10:14:43 -0500

    test consume form url encoded

--- src/main/java/com/crodriguezt/dev/privpasarelaapi/endpoint/WelcomeController.java
+++ src/main/java/com/crodriguezt/dev/privpasarelaapi/endpoint/WelcomeController.java
@@ -1,16 +1,48 @@
 package com.crodriguezt.dev.privpasarelaapi.endpoint;
 
+import java.util.Map;
+
+import org.springframework.http.MediaType;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
-	
-	@RequestMapping(value="/", method=RequestMethod.GET)
-	public String welcome(){
-		
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String welcome() {
+
 		return "Welcome Page";
+	}
+
+	@RequestMapping(value = "/confirmacionPagoPayu", method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+	public String confirmacionPagoPasarela(MultiValueMap  request) {
+		String respuesta = "OK";
+		System.out.println("----->Inicio llamada a WS de Confirmacion:");
+		System.out.println("Map: " + request);
+		System.out.println("----------->detailed Map");
+		//for (String key : request.keySet()) {
+			//System.out.println("--->" + key + ":\t\t\t" + request.get(key));
+		//}
+		System.out.println("to String: " + request.toString());
+		
+		System.out.println("------>Fin print map");
+		return respuesta;
+	}
+	
+	@RequestMapping(value = "/confirmacionPagoPayu2", method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+	public String confirmacionPagoPasarela2(Map<String, Object> request) {
+		String respuesta = "OK";
+		System.out.println("----->Inicio llamada a WS de Confirmacion:");
+		System.out.println("Map: " + request);
+		System.out.println("----------->detailed Map");
+		for (String key : request.keySet()) {
+			System.out.println("--->" + key + ":\t\t\t" + request.get(key));
+		}
+		System.out.println("------>Fin print map");
+		return respuesta;
 	}
 
 }
